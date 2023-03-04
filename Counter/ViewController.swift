04c55803e3ad20c6ff0a344069adede3e244c5e6
Максим Орлов <m4rk184554@yandex.ru>
2023-03-05 01:06:26 +0300

    Добавлена кнопка, меняющая значение счётчика при нажатии

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var counterValue: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    var counter: Int = 0
+    
+    @IBAction func buttonClick(_ sender: Any) {
+        counter += 1
+        counterValue.text = "Значение счётчика: " + String(counter)
+    }
+    
 }
 
